75f46649ab4144f25573ee84fe4fe40407a87bef
Plabrum <plabrum@college.harvard.edu>
2020-09-11 17:16:49 -0400

    navbar and background

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-        return 'Hello, World!'
-#added a new line here to check git on cpanel
+def index():
+        return render_template("index.html")
+        
+if __name__ == '__main__':
+	app.run(debug=True)
+
