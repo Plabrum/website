06498c9358310bb5d19694faf3fb560bae5a4b73
Plabrum <plabrum@college.harvard.edu>
2020-09-30 22:13:19 -0600

    creating a minimum viable product branch

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # website
 Personal Website displaying resume and projects.
+
+Design choices:
+Why SQL alchemy? Why use on ORM at all, why not write at the database connector level? why use mysql over Postgres?
+
+
+This website is likely going change and develop several times as I improve my skills. I want to try and minimize the amount of code rewriting when I want to change an underlying technology.
+
+Framework: In the past I used a django framework, but I wanted a lighter framework for this project - it will likely not need to be as production ready as my last use cases. 
+
+Database: Although my experience is with Postgres databases, I am choosing to use mysql because it comes including in my namecheap hosting plan (I have time to learn but not money to spend)
+
+ORM: I am choosing to use SQL alchemy due to it's cross compatibility across postgres and mysql as well as flask and django. 
+
+With these design choices, the website should be modular enough to change either the framework or database without having to do 'major' rewrites. 
--- app.py
+++ app.py
@@ -4,7 +4,40 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-        return render_template("index.html")
+    return render_template("home.html")
+
+
+# TODO:
+# Put portfolio info on a second page
+'''
+
+@app.route('/portfolio')
+def portfolio():
+	project_list = get_projects()
+	return render_template("portfolio.html", project_list)
+
+'''
+
+# TODO:
+# Ad blog functionality, database, users
+
+'''
+
+@app.route('/blog/<b_slug>')
+def blog():
+	post_slugs = post_slugs_list()
+	if 'b_slug' == "":
+		# serve the listing page if the b_slug is empty (i.e. just clicked the blog page)
+		post_list = post_shortlist()
+		return render_template("blog_listing.html")
+	else if "b_slug" in post_list:
+		# check if slug is the post lists, if yes then show that post
+		post = get_post(b_slug)
+		return render_template("post.html", post)
+	else:
+		# raise error that theres no post with that name, try search
+		return render_template("blog_listing.html", error=("no_slug", "b_slug"))
+'''
         
 if __name__ == '__main__':
 	app.run(debug=True)
