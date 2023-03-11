732de2284573c7553b4eaa09e0c06da6a11c9337
Phil Labrum <philip.labrum@gmail.com>
2023-03-11 12:20:04 -0500

    improved accessibilty

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,42 +17,6 @@ export default function App({ Component, pageProps }: AppProps) {
           nonce: undefined,
         }}
       >
-        <Head>
-          <link
-            id="favicon"
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-          />
-          <link
-            id="favicon"
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16x16.png"
-          />
-          <link
-            id="favicon"
-            rel="apple-touch-icon"
-            href="/apple-touch-icon.png"
-          />
-          <link
-            id="favicon"
-            rel="icon"
-            type="image/png"
-            sizes="192x192"
-            href="/android-chrome-192x192.png"
-          />
-          <link
-            id="favicon"
-            rel="icon"
-            type="image/png"
-            sizes="192x192"
-            href="/android-chrome-512x512.png"
-          />
-          <link rel="manifest" href="/site.webmanifest" />
-        </Head>
         <Component {...pageProps} />
       </GoogleReCaptchaProvider>
     </ThemeProvider>
